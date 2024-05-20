c14e1be60119655461e9c3317a43dc9851dca465
Rosaury Mariana Hernandez <98234894+Rosauher18@users.noreply.github.com>
2024-05-20 17:27:53 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primer numero: ");
+            double firstNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo numero: ");
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Elige operacion (+, -, *, /): ");
+            string operation =  Console.ReadLine();
+
+            double result;
+
+            switch (operation)
+            {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, result);
+                    break;
+                    case "-": 
+                    result = firstNumber - secondNumber;
+                    Console.WriteLine("{0} - {1} = {2}", firstNumber, secondNumber, result);
+                    break;
+                    case "*":
+                    result = firstNumber * secondNumber;
+                    Console.WriteLine("{0} * {1} = {2}", firstNumber, secondNumber, result);
+                    break;
+                    case "/":
+                    if (secondNumber == 0)
+                    {
+                        Console.WriteLine("No se puede dividir por 0");
+                    }
+                    else
+                    {
+                        result = firstNumber / secondNumber;
+                        Console.WriteLine("{0} / {1} = {2}", firstNumber, secondNumber, result);
+
+                    }
+                   break;
+
+                default:
+                    Console.WriteLine("Operacion valida. ");
+                    break;
+
+                 
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
